287412345cf2e822da4ff5433f70dbc803078605
Roblire <123060180+Roblire@users.noreply.github.com>
2023-09-04 13:42:07 +0000

    ConfigConfig

--- JavaScript/p1.js
+++ JavaScript/p1.js
@@ -1,1 +1,6 @@
-console.log("Hello World!")
+console.log("Hello World!");
+
+const descriptionOne = "I am Roblire"
+const descriptionTwo = "I like JavaScript"
+
+console.table([descriptionOne, descriptionTwo])
