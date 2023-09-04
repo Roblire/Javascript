9ddc25a7c0c32f82cc9b0d077bac27eceae90244
Roblire <123060180+Roblire@users.noreply.github.com>
2023-09-04 05:03:37 +0000

    JS

---
+++ JavaScript/p3.js
@@ -0,0 +1,10 @@
+"use strict"; //Treat all JS code into newer versions
+
+// alert(3 + 3) // We are using node Js. not browser
+
+/*
+    console.log(3
+        +
+        3)
+'The above commmand should be written in console.log(3 + 3) for better readability
+*/
