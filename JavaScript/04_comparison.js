9fea05662c2e3700ad24a5abdffc922bc86275ce
Roblire <123060180+Roblire@users.noreply.github.com>
2023-09-06 05:43:25 +0000

    container config done

---
+++ JavaScript/04_comparison.js
@@ -0,0 +1,24 @@
+// console.log(2 > 1); True
+// console.log(2 >= 1); True
+// console.log(2 < 1); False
+// console.log(2 <= 1); False
+// console.log(2 == 1); False
+// console.log(2 != 1); True
+
+/* It converts String into Number and then, compare :-
+    console.log("2" > 1); True
+    console.log("02" > 1); True
+*/
+
+/* It converts null into 0 then, compare :-
+    console.log(null > 0); Fadlse
+    console.log(null == 0); False
+    console.log(null >= 0); True
+*/
+
+// console.log(undefined == 0); False
+// console.log(undefined > 0); False
+// console.log(undefined < 0); False
+
+// Using '==='
+    console.log("2" === 2); // False, as it  strictly checks the datatype
